391614ed590b395efc9dff919fc9dbd5c4377187
Jerre van Veluw <jerrevanveluw@users.noreply.github.com>
2023-05-05 15:34:16 +0200

    fix: typing

--- src/common.ts
+++ src/common.ts
@@ -13,4 +13,4 @@ export const npmViewPackageCommand = (name: string) => ['npm', 'view', name, 've
 
 export const parseWith = (name: string, currentVersion: string) => (data: string) => Promise.resolve(JSON.parse(data))
   .then((it: string | string[]) => Array.isArray(it) ? it : [it])
-  .then((it: any) => [name, currentVersion, it] as const);
+  .then((it: string[]) => [name, currentVersion, it] as const);
